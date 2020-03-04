7bfeb3dc67051254dbab00823eb0c99af37ada59
David Salatic <davidsalatic@live.com>
2020-03-04 14:57:12 +0100

    Class diagram add

--- src/main/java/com/eryce/sportsclub/constants/Permissions.java
+++ src/main/java/com/eryce/sportsclub/constants/Permissions.java
@@ -1,5 +1,7 @@
 package com.eryce.sportsclub.constants;
 
 public class Permissions {
-
+    public static final String ACCESS_MEMBERSHIPS="ACCESS_MEMBERSHIPS";
+    public static final String ACCESS_ALL_TRAINING_SESSIONS="ACCESS_ALL_TRAINING_SESSIONS";
+    public static final String ACCESS_SELF="ACCESS_SELF";
 }
--- src/main/java/com/eryce/sportsclub/constants/Roles.java
+++ src/main/java/com/eryce/sportsclub/constants/Roles.java
@@ -1,7 +1,6 @@
 package com.eryce.sportsclub.constants;
 
 public class Roles {
-
     public static final String CLUB_MEMBER = "CLUB_MEMBER";
     public static final String COACH="COACH";
     public static final String MANAGER="MANAGER";
